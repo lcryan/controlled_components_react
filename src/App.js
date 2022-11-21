534b7410ffeac3f6198f7b4bcb5596c78a24eb8f
LuciaRyan <lcryan06@gmail.com>
2022-11-21 15:32:47 +0100

    set up project started working on form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,48 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
+
+      <>
     <div>
-      Maak hier jouw formulier!
+      <label htmlFor="form">
+          <form id="user form">
+
+         <label htmlFor="first name">Name:</label>
+          <input
+              type="text"
+              id="user name"
+              name="user name"
+              />
+          <label htmlFor="age">Age:</label>
+              <input
+                  type="number"
+                  id="user age"
+                  name="user age"
+                  min="18"
+                  max="100"
+                  />
+              <label htmlFor="message">
+                  <input
+                  type="textarea"
+                  id="user message"
+                  name="user message"
+                  />
+              </label>
+
+
+
+
+
+
+
+          </form>
+
+
+      </label>
     </div>
+    </>
   );
 }
 
